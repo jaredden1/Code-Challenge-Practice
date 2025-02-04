bf7da6ea1f90fabc5f81735591a4b333ce8e3e9b
jaredden1 <james.ifit@gmail.com>
2025-02-03 20:38:39 -0500

    addOneArr
    test passed

---
+++ 5.addOneArr.js
@@ -0,0 +1,6 @@
+// Add one to each number in number array
+const addOneArr = (numberArr) => {
+  return numberArr.map((numberArr) => numberArr + 1);
+};
+let numberArr = [1, 2, 3, 4, 5];
+console.log(addOneArr(numberArr));
