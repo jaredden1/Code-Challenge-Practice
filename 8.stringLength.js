edf80daaf3536f1aafe558620f7055a34504ebf5
jaredden1 <james.ifit@gmail.com>
2025-02-06 18:11:48 -0500

    stringLength
    test passed

---
+++ 8.stringLength.js
@@ -0,0 +1,19 @@
+// Write a function that returns the length of the string inputted without counting the spaces
+const stringLength = (string) => {
+  return string.split(" ").join("").length;
+};
+
+// Test cases
+console.log(stringLength("Hello"));
+console.log(stringLength("How are you doing?"));
+console.log(stringLength("Fine"));
+
+
+// Use Regular Expressions (RegEx) now (/\s/g, "") use with .replace()
+const stringLength1 = (string) => {
+  return string.replace(/\s/g, "").length;
+};
+
+// Test cases
+console.log(stringLength1("Try this"));
+console.log(stringLength1("It works better then split/join!"));
