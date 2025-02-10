491c0766270ce504a0b9a9ae9a0c4eb2499b921c
jaredden1 <james.ifit@gmail.com>
2025-02-10 18:21:27 -0500

    arrayLength
    test passed

---
+++ 9.arrayLength.js
@@ -0,0 +1,7 @@
+// Write a function that returns the length of an array. 
+const arrayLength = () => {
+  let namesArray = ["James", "Zaynah", "Jupiter"];
+  return namesArray.length;
+};
+
+console.log(arrayLength());
