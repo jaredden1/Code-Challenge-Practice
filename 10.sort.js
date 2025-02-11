e2ceeb3e6479dda21f004b20b8dda730fc25e557
jaredden1 <james.ifit@gmail.com>
2025-02-10 19:12:01 -0500

    sort
    test passed

--- 10.sort.js
+++ 10.sort.js
@@ -1,7 +1,15 @@
-// Write a function that sorts an array.
+// Write a function that sorts an array and another for a string.
+
+// String
 const sortArray = () => {
   let letterArray = ["c", "a", "b", "d"];
   return letterArray.sort();
 };
 console.log(sortArray());
 
+// Array
+const sortString = () => {
+  let letterString = "d a b c"
+  return letterString.split("").sort().join("")
+}
+console.log(sortString())
