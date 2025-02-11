e1190564032bf41543ab6a9be9ba24d8f91a7c6d
jaredden1 <james.ifit@gmail.com>
2025-02-10 19:05:21 -0500

    reverse
    test passed

---
+++ 11.reverse.js
@@ -0,0 +1,15 @@
+// Write a function that reverses a string or an array.
+
+// String
+const reverseString = () => {
+  let string = "James";
+  return string.split("").reverse().join("");
+};
+console.log(reverseString());
+
+// Array
+const reverseArray = () => {
+  let arrayName = ["James", "Aaron"];
+  return arrayName.reverse();
+};
+console.log(reverseArray());
